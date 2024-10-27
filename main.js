3938187a0fa9173dd29c5e125764d56e2984e271
Lea Delic <leadelic0429@gmail.com>
2024-10-27 12:31:40 -0400

    add event listener for product selection and quantity input

---
+++ main.js
@@ -0,0 +1,8 @@
+//task 2
+const productSelector = document.getElementById('product-selector');
+const quantityInput = document.getElementById('quantity');
+const totalPriceElement = document.getElementById('total-price');
+const placeOrderButton = document.getElementById('place-order');
+const orderSummary = document.getElementById('order-summary');
+
+
